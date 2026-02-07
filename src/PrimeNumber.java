18130ac9f9220609149d81a6e43457a495d502cb
Virendra-4104 <virendraparwari.4104@gmail.com>
2026-02-07 21:00:07 +0530

    check number is prime or not

---
+++ src/PrimeNumber.java
@@ -0,0 +1,22 @@
+void main() {
+    IO.println("Check Number is Prime or Not.");
+    int number = Integer.parseInt(IO.readln("Enter the number => "));
+    isPrime(number);
+}
+
+public static void isPrime(int num) {
+    if (num <= 1) {
+        IO.println(num + " is not a prime number.");
+        return;
+    }
+    int count = 0;
+    for (int i = 2; i * i <= num; i++)
+        if (num % i == 0){
+            IO.println(num + " is not a prime number.");
+            return;
+        }
+            
+    if (count == 0)
+        IO.println(num + " is a prime number.");
+    
+}
